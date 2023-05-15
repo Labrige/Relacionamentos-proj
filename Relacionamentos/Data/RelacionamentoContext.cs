18bd731cfb438f316abb2ee4e4857f9f0be8fc81
2021102312 <2021102312@LAB401-18>
2023-05-15 20:42:36 -0300

    teste

---
+++ Relacionamentos/Data/RelacionamentoContext.cs
@@ -0,0 +1,16 @@
+﻿using Relacionamentos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Relacionamentos.Data
+{
+    public class RelacionamentoContext : DbContext
+    {
+        public RelacionamentoContext(DbContextOptions<RelacionamentoContext> options) : base(options)
+        {
+        }
+        public DbSet<blog> ?blogs { get; set; }
+        public DbSet<Post> ?posts { get; set; }
+
+    }
+}
